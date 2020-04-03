c5fb77d12397e9e1cf486681adf794486286ee5a
LeiteKaka <viktorbaija1@gmail.com>
2020-04-03 15:16:37 +0200

    Cetvrti komit

--- VeomaOzbiljan/src/Zezanje.java
+++ VeomaOzbiljan/src/Zezanje.java
@@ -3,6 +3,8 @@ public class Zezanje {
 
 	public static void main(String[] args) {
 		System.out.println("Naklapanje");
+		System.out.println();
+		System.out.println("Brbljanje");
 
 	}
 
--- VeomaOzbiljan/src/com/Preozbiljan.java
+++ VeomaOzbiljan/src/com/Preozbiljan.java
@@ -5,6 +5,7 @@ public class Preozbiljan {
 	public static void main(String[] args) {
 	
            System.out.println("Lele dunje ranke!");
+           
 	}
 
 }
